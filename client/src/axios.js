eaa0946b0fdb97422a7fb34459a21549a7a7b40b
Metprox <olegvakarchuk94@gmail.com>
2019-08-07 19:04:40 +0300

    fixing axios and Link router

--- client/src/axios.js
+++ client/src/axios.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 5000;
 
 // Set config defaults when creating the instance
 const Axios = axios.create({
-    baseURL: `https://fierce-lowlands-68726.herokuapp.com:${PORT}`,
-    // baseURL: `http://localhost:5000`,
+    // baseURL: `https://fierce-lowlands-68726.herokuapp.com:${PORT}`,
+    baseURL: `http://localhost:5000`,
 });
 
 export default Axios;
